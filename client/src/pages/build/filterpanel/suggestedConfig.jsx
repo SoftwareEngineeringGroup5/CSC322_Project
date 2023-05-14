80d5873d15aa9b5be472c2c06a872b974b7a9cd3
mhshabbir <shabbir.muhammad@outlook.com>
2023-05-13 23:29:10 -0400

    updating my git files

--- client/src/pages/build/filterpanel/suggestedConfig.jsx
+++ client/src/pages/build/filterpanel/suggestedConfig.jsx
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-import React from 'react'
-import { SUGGESTEDCOFIGS } from '../SuggestedConfigData'
 
-
-export const SuggestedConfig = (props) => {
-  const {id, configName} = props.data
-
-  return (
-    <p>
-      {configName}
-    </p>
-=======
 import React, { useContext } from 'react'
 import { ShopContext } from '../../../context/shop-context'
 
@@ -22,6 +10,5 @@ export const SuggestedConfig = (props) => {
   return (
     <button className='suggested-button' onClick={() => setBundleThroughSuggestions(selectedProducts, rating, bundledproductsuggest)} >{configName}</button>
       
->>>>>>> 55539d5ccb60ba04b600facf4476f2d223630366
   )
 }
